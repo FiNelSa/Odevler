78c61125126c40e123db83b04e2f4647fd18d3ba
FiNelSa <finelsa97@gmail.com>
2024-10-12 17:52:15 +0300

    yeni ödevler

---
+++ Islemler/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Dört İşlem
+
+            int birinciSayi = 72;
+            int ikinciSayi = 24;
+
+                System.out.println(birinciSayi*ikinciSayi);
+                System.out.println(birinciSayi/ikinciSayi);
+                System.out.println(birinciSayi+ikinciSayi);
+                System.out.println(birinciSayi-ikinciSayi);
+
+
+        //Üçgenin Alanı ve Çevresi
+
+            int taban = 7;
+            int yukseklik = 20;
+
+                System.out.println((taban*yukseklik)/2);
+                System.out.println(taban*3);
+
+         //Dairenin Alanı ve Çevresi
+
+            int yariCap = 13;
+
+                System.out.println((yariCap^2)*3);
+                System.out.println(yariCap*2*3);
+    }
+}
