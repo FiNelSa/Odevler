fb0ea0600cf10c6c9ce7681b0e30c7fffa572bab
FiNelSa <finelsa97@gmail.com>
2024-10-12 18:14:05 +0300

    ödev

--- String Ödev/src/Main.java
+++ String Ödev/src/Main.java
@@ -1,9 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        int sinifErkekOgrenci =13;
-        int sinifKizOgrenci =15;
-        int toplamOgrenci = sinifKizOgrenci + sinifErkekOgrenci;
-        System.out.println("1. Soru \n Herksin ismi olan bir kurada kaç kere kura yapılırsa en az bir erkek öğrenci çekilir?");
-        System.out.println(toplamOgrenci - sinifErkekOgrenci +1);
+
+        //String Senaryo
+
+       String name1 = "Semih";
+       String name2 = "Ufuk";
+       String name3 = "Efe";
+
+       int lname1 = name1.length();
+       int lname2 = name2.length();
+       int lname3 = name3.length();
+
+       if (lname1 > lname2){
+           if (lname1 > lname3) {
+               System.out.println(name1 + " ismi " + name2 + " isminden ve " + name3 + " isminden uzundur.");
+           }else {
+               System.out.println(name3 + " ismi " + name1 + " isminden, o da " + name2 + " isminden uzundur.");
+           }
+       }else if (lname2 > lname3){
+           System.out.println(name2 + " ismi " + name1 + " isminden ve " + name3 + " isminden uzundur.");
+       }else {
+           System.out.println(name3 + " ismi " + name2 + " isminden, o da " + name1 + " isminden uzundur.");
+       }
+
     }
 }
