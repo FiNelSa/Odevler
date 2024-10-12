a075c947ab41867c40ff2860ed0d73c969dc9315
FiNelSa <finelsa97@gmail.com>
2024-10-12 17:43:11 +0300

    Ödevler

---
+++ String Ödev/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int sinifErkekOgrenci =13;
+        int sinifKizOgrenci =15;
+        int toplamOgrenci = sinifKizOgrenci + sinifErkekOgrenci;
+        System.out.println("1. Soru \n Herksin ismi olan bir kurada kaç kere kura yapılırsa en az bir erkek öğrenci çekilir?");
+        System.out.println(toplamOgrenci - sinifErkekOgrenci +1);
+    }
+}
