97de8ef1dce08cad9d57eab2be70177d550633f8
FiNelSa <finelsa97@gmail.com>
2024-10-20 20:42:02 +0300

    java

---
+++ Islemler/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+
+        //İki Sayı ile 4 Aritmetik İşlem
+        int sayiBir = scan.nextInt();
+        int sayiIki = scan.nextInt();
+        System.out.println(sayiBir+sayiIki);
+        System.out.println(sayiBir-sayiIki);
+        System.out.println(sayiBir/sayiIki);
+        System.out.println(sayiBir*sayiIki);
+
+        //Bir Eşkenar üçgenin alanı ve çevresi
+        int yukseklik = scan.nextInt();
+        int taban = scan.nextInt();
+        System.out.println((yukseklik*taban)/2);
+        System.out.println(taban*3);
+
+        //Dairenin Alanı ve Çapı
+        int r = scan.nextInt();
+        int p = 3;
+        System.out.println((r^2)*p);
+        System.out.println(r*2*p);
+    }
+}
